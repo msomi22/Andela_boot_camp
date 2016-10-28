661bef770ec75faed295862e45eadc6114369544
msomi22 <mwendapeter72@gmail.com>
2016-10-28 18:34:11 +0300

    changes in prime number finder

--- prime_number.py
+++ prime_number.py
@@ -3,19 +3,29 @@ A prime number that is only devisible by one and it self
 Example:
 1,3,5,7,11,13,17,19,23 and so one
 """
-def prime_finder(endpoint):
-	start = 2
-	while start <= endpoint:
-	    is_prime=True
-	    for i in range(2, start):
-	        if start%i == 0:
-	            is_prime=False
-	            break;
-	    if is_prime==True:
-	        print start
-	    start = start+1
-	
+def prime_finder(endpoint): 
+	if endpoint == 2:
+		return [2]
+	elif endpoint < 2:
+		return []
 
-#prime_finder(100)    
+	first=range(3,endpoint+1,2)
+	last = endpoint ** 0.5
+	nextno=(endpoint+1)/2-1
+
+	i=0
+	k=3
+	while k <= last:
+		if first[i]:
+			j=(k*k-3)/2
+			first[j]=0
+
+			while j<nextno:
+				first[j]=0
+				j+=k
+		i=i+1
+		k=2*i+3
+
+	return [2]+[num for num in first if num] 
        
    
--- test_prime_number.py
+++ test_prime_number.py
@@ -5,4 +5,7 @@ Test case for prime_number_finder
 """
 class prime_number_finder_test(unittest.TestCase):
 	def testprime(self):
-		assertEquals(prime_number.prime_finder(100),[])
+		self.assertEquals(prime_number.prime_finder(40),[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
+
+if __name__ == '__main__':
+    unittest.main()    		
